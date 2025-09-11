740c174b4108f95361fab2f151b631f710152e33
Thalita Andrade <34509079+thalitaandrade@users.noreply.github.com>
2025-09-11 20:32:40 -0300

    Update DataController.java

--- src/main/java/com/dev/cicd/data/controller/DataController.java
+++ src/main/java/com/dev/cicd/data/controller/DataController.java
@@ -18,7 +18,7 @@ public class DataController {
 
         @GetMapping("/version")
         public String version() {
-                return "The actual version is 2.0.0";
+                return "The actual version is 2.1.0";
         }
 
         @GetMapping("/nations")
